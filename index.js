a875cbc8231853b39fe5c5d23621ac3cfa5f6333
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-31 17:26:04 -0500

    commit tree 22f220af0bd5305681b41d7288096fe18efb3f83

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, test){
+    let result = array.filter(string => string.toLowerCase() === test.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(array, test){
+    let count = test.length;
+    let result = array.filter(string => string.slice(0, count) === test);
+    return result;
+}
+
+function matchName(array, test){
+    let result = array.filter(string => string.name === test)
+    return result
+
+}
